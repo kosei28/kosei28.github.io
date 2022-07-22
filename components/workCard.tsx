27b8db2c3cab3ca1c0519ef14a5692999c30fbc9
kosei28 <niwa.kosei@gmail.com>
2022-07-23 04:16:40 +0900

    deploy to github pages

--- components/workCard.tsx
+++ components/workCard.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+/* eslint-disable @next/next/no-img-element */
 
 type Props = {
   name: string;
@@ -12,9 +12,11 @@ export const WorkCard: React.FC<Props> = ({ name, description, url, image }) => 
     <div>
       <a href={url} target="_blank" rel="noreferrer">
         <div className="flex flex-col overflow-hidden rounded-xl border shadow sm:flex-row">
-          <div className="relative h-48 w-auto shrink-0 sm:h-auto sm:w-60">
-            <Image src={image} layout="fill" objectFit="cover" alt={name} />
-          </div>
+          <img
+            src={image}
+            alt={name}
+            className="h-48 w-auto shrink-0 object-cover sm:h-auto sm:w-60"
+          />
           <div className="p-4">
             <h2 className="mb-4 text-2xl">{name}</h2>
             <p className="text-lg">{description}</p>
